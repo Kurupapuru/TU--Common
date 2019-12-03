8552535ab2efbd5a724fa23f020710c4fdbf2b51
TeterinDY-plavalaguna <teterindy@plavalaguna.ru>
2019-12-03 20:55:24 +0300

    Triggers Collector and DictionaryExtension for force get classes

---
+++ TU/Sharp/Extensions/DictionaryExtension.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Code.Extensions
+{
+    public static class DictionaryExtension
+    {
+        public static Value Get<Key, Value>(this Dictionary<Key, Value> source, Key key, Func<Value> defaultValue)
+        {
+            if (source.TryGetValue(key, out var result))
+            {
+                return result;
+            }
+            else
+            {
+                var createdDefaultValue = defaultValue.Invoke();
+                source[key] = createdDefaultValue;
+                return createdDefaultValue;
+            }
+        }
+    }
+}
---
+++ TU/Unity/ComponentUtils/TriggersCollector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggersCollector : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    [NonSerialized] public List<Collider> colliders = new List<Collider>(1);
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!layerMask.LayerCheck(other.gameObject.layer)) return;
+        colliders.Add(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!layerMask.LayerCheck(other.gameObject.layer)) return;
+        colliders.Remove(other);
+    }
+}
