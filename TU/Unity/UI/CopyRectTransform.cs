783ca0bd86ff8f5f4b8f70bb8f6be70f60fe6496
DaniilTeterin <danteterin@mail.ru>
2020-01-17 06:25:59 +0300

    UI Components

---
+++ TU/Unity/UI/CopyRectTransform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using TU.Sharp.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CopyRectTransform : MonoBehaviourInvokable
+{
+    public RectTransform fromT;
+    public RectTransform toT;
+
+    [Button]
+    public override void Invoke()
+    {
+        toT.anchoredPosition = fromT.anchoredPosition;
+        toT.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,   fromT.rect.height);
+        toT.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, fromT.rect.width);
+    }
+}
---
+++ TU/Unity/UI/SizeFromChilds.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using Sirenix.OdinInspector;
+using TU.Sharp.Utils;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))] [ExecuteInEditMode]
+public class SizeFromChilds : MonoBehaviourInvokable
+{
+    public RectTransform setSizeTo;
+    public RectTransform readSizeFrom;
+    public bool onUpdate   = false;
+    public bool copyHeight = false;
+    public bool copyWidth  = false;
+    public bool сheckIgnoreLayout = false;
+
+    private void Update()
+    {
+        if (onUpdate)
+            Invoke();
+    }
+
+    [Button]
+    public override void Invoke()
+    {
+        if (copyHeight)
+        {
+            float heightSum = 0;
+            foreach (RectTransform child in readSizeFrom)
+                if (!isIgnoreLayout(child)) heightSum += child.rect.height;
+            setSizeTo.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, heightSum);
+        }
+        
+        if (copyWidth)
+        {
+            float widthSum = 0;
+            foreach (RectTransform child in readSizeFrom)
+                if (!isIgnoreLayout(child)) widthSum += child.rect.width;
+            setSizeTo.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, widthSum);
+        }
+    }
+
+    private bool isIgnoreLayout(RectTransform rectTransform)
+    {
+        if (сheckIgnoreLayout == false) return false;
+
+        var layoutElementComponent = rectTransform.GetComponent<LayoutElement>();
+        if (layoutElementComponent == null) return false;
+
+        return layoutElementComponent.ignoreLayout;
+    }
+}
