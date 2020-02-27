44bf0e046a5c26c10a7e5fa46081c71b4e094fee
DaniilTeterin <danteterin@mail.ru>
2020-02-27 11:56:07 +0300

    Unity Extensions

---
+++ TU/Unity/UnityEngineExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace TU.Unity
+{
+    public static class UnityEngineExtensions
+    {
+        public static bool IsNull(this UnityEngine.Object source) => ReferenceEquals(source, null);
+    }
+}
