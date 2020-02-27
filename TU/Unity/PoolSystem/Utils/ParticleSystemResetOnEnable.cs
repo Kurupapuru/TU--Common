a04a1f24bedea15a1d637af4524903548258b501
DaniilTeterin <danteterin@mail.ru>
2020-02-27 11:54:49 +0300

    PoolSystem

---
+++ TU/Unity/PoolSystem/Utils/ParticleSystemResetOnEnable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemResetOnEnable : MonoBehaviour
+{
+    private ParticleSystem ps;
+    
+    private void OnEnable()
+    {
+        ps.Stop(true);
+        ps.Play(true);
+    }
+}
