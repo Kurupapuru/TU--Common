36837fb090d1bd6584a2179e8b2c994be0359c00
DaniilTeterin <danteterin@mail.ru>
2020-04-03 17:58:50 +0300

    Character, Input

---
+++ TU/Unity/Input/RayEngine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Lean.Touch;
+using UnicornLib.Input;
+using UnityEngine;
+
+// ReSharper disable DelegateSubtraction
+
+public class RayEngine : MonoBehaviour
+{
+    private const int MaxDistance = 30;
+
+
+    public static RayEngine instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    #region RayHandle
+
+    public void OnEnable()
+    {
+        LeanTouch.OnFingerDown += OnFingerDown;
+        LeanTouch.OnFingerUp   += OnFingerUp;
+        LeanTouch.OnFingerTap  += OnFingerTap;
+    }
+
+    public void OnDisable()
+    {
+        LeanTouch.OnFingerDown -= OnFingerDown;
+        LeanTouch.OnFingerUp   -= OnFingerUp;
+        LeanTouch.OnFingerTap  -= OnFingerTap;
+    }
+
+    private void OnFingerDown(LeanFinger finger) =>
+        FingerActionHandle<ITouchDownObserver>(finger, (observer, touch) => observer.OnTouchDown(touch));
+
+    private void OnFingerUp(LeanFinger finger) =>
+        FingerActionHandle<ITouchUpObserver>(finger, (observer, touch) => observer.OnTouchUp(touch));
+
+    private void OnFingerTap(LeanFinger finger) =>
+        FingerActionHandle<ITapObserver>(finger, (observer, touch) => observer.OnTap(touch));
+
+    private void FingerActionHandle<T>(LeanFinger touch, Action<T, LeanFinger> doForEachObserver)
+    {
+        RaycastHit[] hits = RaycastHelper.GetRayHitAll(touch.ScreenPosition, StaticValues.Interactable.layerMask);
+
+        foreach (RaycastHit hit in hits)
+        {
+            T[] observers;
+            if (hit.rigidbody != null)
+                observers = hit.rigidbody.GetComponents<T>();
+            else
+                observers = hit.collider.GetComponents<T>();
+
+            foreach (var observer in observers)
+                doForEachObserver.Invoke(observer, touch);
+        }
+    }
+
+    #endregion RayHandle
+
+    #region Static / GetRay
+
+    
+
+    #endregion Static / GetRay
+}
+
+//Ray Interfaces
+public interface ITouchDownObserver
+{
+    void OnTouchDown(LeanFinger touch);
+}
+
+public interface ITouchUpObserver
+{
+    void OnTouchUp(LeanFinger touch);
+}
+
+public interface ITapObserver
+{
+    void OnTap(LeanFinger touch);
+}
---
+++ TU/Unity/Physics/RigidbodyImprover.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using TU.Unity.Extensions;
+using UnityEngine;
+
+namespace GameDynamics.Character
+{
+    /// <summary>
+    /// For proper working this script needs movementDirection setted from movement controller script
+    /// </summary>
+    public class RigidbodyImprover : MonoBehaviour
+    {
+        [Header("References")]
+        public Rigidbody selfRb;
+        public Collider selfCollider;
+
+        [Header("Settings")]
+        public LayerMask groundLayerMask;
+        public LayerMask stairsLayerMask;
+        public float slopeAngle = 35;
+        public PhysicMaterial onGroundMaterial, onSlopeMaterial;
+        public float stepHeight = .35f;
+        
+        
+        // TODO: Show this values in "Debug" group
+        [NonSerialized] public bool IsOnGround;
+        [NonSerialized] public bool IsOnSlope;
+        [NonSerialized] public Vector3 movementDelta;
+        [NonSerialized] public Vector3 movementDirection;
+        public bool IsCanWalk => IsOnGround && !IsOnSlope;
+        
+        
+        private Vector3 previousPosition;
+
+        private void Start() { previousPosition = selfRb.position; }
+
+        private void FixedUpdate()
+        {
+            RaycastHit groundRaycastHit;
+            Physics.Raycast(
+                new Ray(selfRb.position + Vector3.up * .05f, Vector3.down),
+                out groundRaycastHit,
+            2,
+                groundLayerMask);
+            
+            
+            MovementDeltaUpdate();
+            IsOnGroundCheck(groundRaycastHit);
+            IsOnSlopeCheck(groundRaycastHit);
+            MaterialByIsOnSlope();
+
+        }
+
+        private void MovementDeltaUpdate()
+        {
+            movementDelta = selfRb.position - previousPosition; 
+            previousPosition = selfRb.position;
+        }
+
+        private void IsOnGroundCheck(RaycastHit hit)
+        {
+            IsOnGround = hit.collider != null && hit.distance < .2f;
+        }
+
+        private void IsOnSlopeCheck(RaycastHit hit)
+        {
+            var hitAngle = Vector3.Angle(Vector3.up, hit.normal);
+            Debug.DrawRay(hit.point, hit.normal, Color.red, .1f, false);
+            IsOnSlope = hit.collider != null 
+                        && 
+                        hitAngle >= slopeAngle;
+            Debug.Log(hitAngle);
+        }
+
+        private void MaterialByIsOnSlope()
+        {
+            selfCollider.sharedMaterial = IsOnSlope ? onSlopeMaterial : onGroundMaterial;
+        }
+        
+        
+        
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            ClimbOnStep(other);
+        }
+
+        private void ClimbOnStep(Collision other)
+        {
+            if (!stairsLayerMask.LayerCheck(other.gameObject.layer))
+                return;
+                
+            var canClimbTo = selfRb.position.y + stepHeight;
+            var isCanClimb = true;
+            float highestPoint = selfRb.position.y;
+            foreach (var contact in other.contacts)
+            {
+                if (contact.point.y > canClimbTo
+                    ||
+                    movementDirection == Vector3.zero 
+                    || 
+                    Vector3.Dot(movementDirection.normalized, (contact.point - selfRb.position).normalized) < .25f) // Слишком высокая ступенька или персонаж не идет в ее направлении
+                {
+                    isCanClimb = false;
+                    break;
+                }
+
+                if (highestPoint < contact.point.y) highestPoint = contact.point.y;
+            }
+
+            if (!isCanClimb) return;
+            
+            selfRb.MovePosition(new Vector3(selfRb.position.x, highestPoint, selfRb.position.z));
+        }
+    }
+}
